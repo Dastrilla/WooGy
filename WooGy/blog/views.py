710c6e869c46fc601a18115ae642487e38949992
Dastrilla <dastrilla@yandex.ru>
2024-09-24 11:07:31 +1100

    add paginator, add share form, and share to email

--- WooGy/blog/views.py
+++ WooGy/blog/views.py
@@ -1,17 +1,77 @@
-from django.shortcuts import get_object_or_404,render
+from django.core.mail import send_mail
+from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
+from django.shortcuts import get_object_or_404, render
 from .models import Post
+from .forms import MailPostForm
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except:
+        posts = paginator.page(paginator.num_pages)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 
-def post_detail(request, id):
+def post_detail(request,year, month, day, post):
     post = get_object_or_404(
         Post,
-        id = id,
-        status = Post.Status.PUBLISHED
+        status = Post.Status.PUBLISHED,
+        slug = post,
+        publish__year = year,
+        publish__month = month,
+        publish__day = day
     )
     
     return render(request, 'blog/post/detail.html', {'post':post})
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    sent = False
+
+    if request.method == "POST":
+        form = MailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+            post.get_absolute_url()
+            )
+        subject = (
+        f"{cd['name']} ({cd['email']}) "
+        f"recomends your read {post.title}"
+        )
+        message = (
+            f"Read {post.title} at {post_url}\n\n"
+            f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+        send_mail(
+            subject=subject,
+            message=message,
+            from_email=None,
+            recipient_list=[cd['to']]
+        )
+        sent = True
+    else:
+        form = MailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post':post,
+            'form':form,
+            'sent':sent
+        }
+    ) 
