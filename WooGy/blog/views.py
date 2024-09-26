cefb11e0fdcb5eabc5dd4ec4cb38e69de6d7ddaf
Dastrilla <dastrilla@yandex.ru>
2024-09-27 10:43:44 +1100

    add search fuctionality

--- WooGy/blog/views.py
+++ WooGy/blog/views.py
@@ -1,8 +1,9 @@
 from django.core.mail import send_mail
+from django.contrib.postgres.search import SearchVector
 from django.core.paginator import EmptyPage, Paginator, PageNotAnInteger
 from django.shortcuts import get_object_or_404, render
 from .models import Post
-from .forms import MailPostForm, CommentForm
+from .forms import MailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
 
@@ -136,3 +137,29 @@ def post_comment(request, post_id):
             'comment':comment                
         }
         )
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = (
+                Post.published.annotate(
+                    search=SearchVector('title', 'body'),
+                    )
+                .filter(search=query)
+            )
+
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results
+        }
+    )
